ac9d3b2ddb7e2c75d44e2eda1077c08820f4456b
jiHeeFlee <esder1310@naver.com>
2024-11-02 15:25:28 +0900

    setting : routes, theme, pages

---
+++ src/pages/Login.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import themeGet from '../utils/themeGet';
+
+const Container=styled.div`
+    background-color: ${themeGet('color.green')};
+    color:${themeGet('color.dark')};
+    font-size: ${themeGet('font.h1.size')};
+    font-weight: ${themeGet('font.h1.weight')};
+`;
+
+function Login(){
+
+    return(
+        <>
+            <Container>
+                <h1>login page</h1>
+            </Container>
+        </>
+    )
+}
+
+export default Login;
