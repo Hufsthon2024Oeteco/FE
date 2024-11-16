53cd8eb3f59755493fec35cd5db89869fce0f881
yerimi00 <fhddl1019@gmail.com>
2024-11-16 14:32:35 +0900

    feat : area 드롭다운 css 작업 완료

--- src/pages/mapPage/AreaInput.js
+++ src/pages/mapPage/AreaInput.js
@@ -8,7 +8,15 @@ import { BsCaretDownFill } from "react-icons/bs";
 import { useState } from "react";
 
 function AreaInput() {
-  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedValues, setSelectedValues] = useState({
+    first: "",
+    second: "",
+    third: "",
+  });
+
+  const handleSelectChange = (level, value) => {
+    setSelectedValues((prev) => ({ ...prev, [level]: value }));
+  };
 
   return (
     <Wrapper>
@@ -16,17 +24,45 @@ function AreaInput() {
         궁금한 지역의 위치를 알려주세요.
       </CustomFont>
       <SelectContainer>
-        <StyledSelect>
-          <option>시 / 시(자치시, 행정시)</option>
-          <option>시 / 시2222(자치시, 행정시)</option>
-          <option>시 / 시(3333자치시, 행정시)</option>
-        </StyledSelect>
-        <StyledSelect>
-          <option>구(자치구, 일반구)</option>
-        </StyledSelect>
-        <StyledSelect>
-          <option>읍·면·행정동</option>
-        </StyledSelect>
+        <SelectWrapper>
+          <StyledSelect
+            value={selectedValues.first}
+            onChange={(e) => handleSelectChange("first", e.target.value)}
+          >
+            <option value="">시 / 시(자치시, 행정시)</option>
+            <option value="option1">시 / 시2222(자치시, 행정시)</option>
+            <option value="option2">시 / 시(3333자치시, 행정시)</option>
+          </StyledSelect>
+          <IconWrapper selected={selectedValues.first !== ""}>
+            <BsCaretDownFill size={"24px"} />
+          </IconWrapper>
+        </SelectWrapper>
+        <SelectWrapper>
+          <StyledSelect
+            value={selectedValues.second}
+            onChange={(e) => handleSelectChange("second", e.target.value)}
+          >
+            <option value="">구(자치구, 일반구)</option>
+            <option value="option1">구1</option>
+            <option value="option2">구2</option>
+          </StyledSelect>
+          <IconWrapper selected={selectedValues.second !== ""}>
+            <BsCaretDownFill size={"24px"} />
+          </IconWrapper>
+        </SelectWrapper>
+        <SelectWrapper>
+          <StyledSelect
+            value={selectedValues.third}
+            onChange={(e) => handleSelectChange("third", e.target.value)}
+          >
+            <option value="">읍·면·행정동</option>
+            <option value="option1">읍/면/행정동1</option>
+            <option value="option2">읍/면/행정동2</option>
+          </StyledSelect>
+          <IconWrapper selected={selectedValues.third !== ""}>
+            <BsCaretDownFill size={"24px"} />
+          </IconWrapper>
+        </SelectWrapper>
       </SelectContainer>
       <BottomButton>확인하기</BottomButton>
     </Wrapper>
@@ -58,14 +94,20 @@ const SelectContainer = styled.div`
   gap: 37px;
 `;
 
+const SelectWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
 const StyledSelect = styled.select`
   width: 100%;
   padding: 18px 20px;
   font-size: 18px;
   font-weight: bold;
-  color: ${(props) => (props.value ? "#fff" : "#8e8e8e")};
-  background-color: ${(props) => (props.value ? "#00c985" : "#fff")};
-  border: 1px solid #d9d9d9;
+  color: ${(props) => (props.value ? "#00d4ac" : "#8e8e8e")};
+  background-color: #fff;
+  border: ${(props) =>
+    props.value ? "3px solid #00d4ac" : "3px solid #d9d9d9"};
   border-radius: 25px;
   outline: none;
   appearance: none;
@@ -73,12 +115,16 @@ const StyledSelect = styled.select`
   -moz-appearance: none;
   cursor: pointer;
 
-  background-image: BsCaretDownFill;
-  background-repeat: no-repeat;
-  background-position: right 15px center;
-  background-size: 12px;
-
   &:focus {
     border-color: #00c985;
   }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+  color: ${(props) => (props.selected ? "#00d4ac" : "#8e8e8e")};
 `;
