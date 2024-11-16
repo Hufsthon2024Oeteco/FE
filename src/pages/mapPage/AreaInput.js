d8b8265c30f9aca78e21e22f67710749f08d2857
yerimi00 <fhddl1019@gmail.com>
2024-11-16 13:09:41 +0900

    feat : 라우트 설정

---
+++ src/pages/mapPage/AreaInput.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import themeGet from "../../utils/themeGet";
+
+import CustomColumn from "../../components/CustomColumn";
+import CustomFont from "../../components/CustomFont";
+
+function AreaInput() {
+  return <div>지역입력페이지</div>;
+}
+
+export default AreaInput;
--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,35 @@
-import Main from './pages/mainpage/Main';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
+import Main from "./pages/mainpage/Main";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import AreaInput from "./pages/mapPage/AreaInput";
+import MapPage from "./pages/mapPage/MapPage";
 
 const routes = [
-    {
-        path: '/',
-        element: <Main />,
-        name: '메인 페이지'
-    },
-    {
-        path: '/login',
-        element: <Login />,
-        name: '로그인 페이지'
-    },
-    {
-        path: '/signup',
-        element: <Signup />,
-        name: '회원가입 페이지'
-    }
-]
+  {
+    path: "/",
+    element: <Main />,
+    name: "메인 페이지",
+  },
+  {
+    path: "/area",
+    element: <AreaInput />,
+    name: "지역입력",
+  },
+  {
+    path: "/area/map",
+    element: <MapPage />,
+    name: "지역입력",
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    name: "로그인 페이지",
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    name: "회원가입 페이지",
+  },
+];
 
 export default routes;
